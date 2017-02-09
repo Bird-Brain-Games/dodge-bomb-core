c6602a86bb5031d5899260b84fc351d6185ae147
geekid47 <geekid47@hotmail.com>
2017-02-09 00:34:44 -0500

    really, reallly buggy throw code. Messed up rotation? see details bomb offset working but there is a disconect between bomb and robot rotation. Impulse force is not reseting hard to tell whether impulse is working properly.
    The bombot orientation/rotation is weird? wont rotate 360 degrees pops from
    like 320 to 400.
    ps. campbell is dead dead, be back soon

--- src/material.h
+++ src/material.h
@@ -13,8 +13,8 @@ public:
 	//our materials shader
 	std::shared_ptr<Shader> shader;
 	// the unifrom data
-	std::map<std::string, glm::vec4, int> vec4Uniforms;
-	std::map<std::string, glm::mat4, int> mat4Uniforms;
+	std::map<std::string, glm::vec4> vec4Uniforms;
+	std::map<std::string, glm::mat4> mat4Uniforms;
 
 	Material();
 	//sends the uniform data to our shader
