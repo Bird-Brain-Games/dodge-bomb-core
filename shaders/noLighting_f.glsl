3992bda91ccab5b55374fee0230c4fd95d0bf611
rsavaglio <rsavaglio@localhost>
2017-03-06 14:56:42 -0500

    only texture, no light

---
+++ shaders/noLighting_f.glsl
@@ -0,0 +1,24 @@
+#version 400
+
+uniform vec4 u_colour;
+
+uniform sampler2D u_diffuseTex;
+uniform sampler2D u_specularTex;
+
+// Fragment Shader Inputs
+in VertexData
+{
+	vec3 normal;
+	vec3 texCoord;
+	vec4 colour;
+	vec3 posEye;
+} vIn;
+
+layout(location = 0) out vec4 FragColor;
+
+void main()
+{
+	vec4 diffuseColour = texture2D(u_diffuseTex, vIn.texCoord.st);
+
+	FragColor = vec4(diffuseColour);
+}
