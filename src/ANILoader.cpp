9c98f0e0771147b810a8a47a873910d8c83554f1
geekid47 <35320547+campbellHamilton@users.noreply.github.com>
2017-03-11 20:10:21 -0500

    Switched to loading multiple animations instead of loading 1 and using it for all bombots.

--- src/ANILoader.cpp
+++ src/ANILoader.cpp
@@ -614,3 +614,12 @@ void Holder::setAnim(std::string _name)
 	else
 		std::cout << "error the animation " + _name + " does not exist" << std::endl;
 }
+
+void loadAnimations(std::shared_ptr<Holder> _temp)
+{
+	_temp->baseLoad("Assets/htr/bombot");
+	_temp->AniLoad("Assets/htr/idle", "idle");
+	_temp->AniLoad("Assets/htr/throw", "throw");
+	_temp->AniLoad("Assets/htr/walk", "walk");
+	_temp->setAnim("idle");
+}
