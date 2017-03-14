1ad69bff17dcb42a7ce2ce0e31585acb5ab9c7ee
Graham Watson <12705535+Goombario@users.noreply.github.com>
2017-03-13 20:08:17 -0400

    gamestate resizes window

---
+++ src/ANILoader.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <memory>
+#include <string>
+#include "Node.h"
+#include "JointTypes.h"
+#include "loader.h"
+#include <map>
+class ANILoader
+{
+public:
+	ANILoader();
+
+	// Loads the specified HTR file. 
+	// Returns true if successful
+	bool loadHTR(std::string fileName);
+
+	// Loop through each joint descriptor until the input jointName is found
+	// If not found, will return a nullptr
+	JointDescriptor* getJointDescriptorByName(std::string jointName);
+
+	// Returns pointer to specific joint
+	Node* getNodeByName(std::string jointName);
+
+	// Turns the HTR file into usable game objects
+	void createNodes();
+
+	// Returns root node (usually the hip if a humanoid skeleton)
+	Node* getRootNode();
+
+	//used to get some values.
+	std::vector<glm::vec3> getVertexs();
+	std::vector<glm::vec2> getUV();
+	std::vector<glm::vec3> getNormals();
+	std::vector<glm::vec4> getWeights();//the weights for each vertex
+	std::vector<glm::vec4> getJoints();//the joint for each vertex
+
+	//used to get data.
+	glm::vec3* ANILoader::getVertexsData();
+	glm::vec2* ANILoader::getUVData();
+	glm::vec3* ANILoader::getNormalsData();
+	glm::vec4* ANILoader::getWeightsData();
+	glm::vec4* ANILoader::getJointsData();
+
+	int getSegments();
+
+private:
+
+	// Functions used in parsing process
+	bool processHeaderSection(FILE* fp, char* loc);
+	bool processHeader(std::string header, std::string value);
+	bool processSegementNameSection(FILE* fp, char* loc);
+	bool processBasePositionSection(FILE* fp, char* loc);
+	bool processAnimationSection(FILE* fp, char* loc);
+	bool processVBO(FILE* fp, char* loc);
+
+	// Increments the file pointer to the next line in the file
+	// and copies it into buffer
+	void goToNextValidLineInFile(FILE* fp, char* buffer);
+
+	// Takes in 3 euler angles and returns a quaternion
+	glm::quat createQuaternion(float rx, float ry, float rz);
+
+	// Describes the data in the file
+
+	std::string fileType;		// File extension
+	std::string dataType;		// What kind of data is stored in file. 
+								// Ie. HTRS means Hierarchical translations followed by rotations and scale
+
+	int			fileVersion;	// Useful if you are generating binary object files on load, can check the version of an existing binary file, 
+								// check version of text, if text version > binary version then reparse, otherwise just use binary
+
+	int			numSegments;	// Number of bones in skeleton
+	int			numFrames;		// number of frames in the animation
+	int			fps;			// FPS of the animation
+
+	RotationOrder rotationOrder;// Order to apply rotations
+	std::string	calibrationUnits;// 
+	char		upAxis;			// X, Y, Z (usually Y)
+	std::string rotationUnits;	// "Degrees" or "Radians"
+	char		boneLengthAxis;	// Axis that is aligned with bone
+
+	// Actual animation data
+	std::vector<JointDescriptor> jointDescriptors;	// Stores each joint and its parent (could be an array since we know number of segments)
+
+	std::vector<Node> jointNodes;
+
+	std::vector<glm::vec3> vertexsOrder;
+	std::vector<glm::vec2> UVOrder;
+	std::vector<glm::vec3> normalsOrder;
+	std::vector<glm::vec4> weightsOrder;//the weights for each vertex
+	std::vector<glm::vec4> jointsOrder;//the joint for each vertex
+
+	std::vector<glm::vec3> vertexs;
+	std::vector<glm::vec2> UV;
+	std::vector<glm::vec3> normals;
+	std::vector<glm::vec4> weights;//the weights for each vertex
+	std::vector<glm::vec4> joints;//the joint for each vertex
+
+	Node* rootNode;
+};
+
+class Holder : public Loader
+{
+private:
+
+	Node* basePose;
+	std::map<std::string, Node*> animations;
+	Node* currentTop;
+	Node* currentBot;
+	unsigned int numtris; // count number of vertices for data creation
+	int bones;
+
+	// the base pose matricies
+	std::vector<glm::mat4> matricies;
+	// the current transformation matrix
+	std::vector<glm::mat4> multipliedMatricies;
+	void createVAO();
+
+	float angle;
+public:
+	//
+	Holder() {};
+	Holder(std::shared_ptr<Holder>);
+	void setAnim(std::string);
+	bool baseLoad(std::string);
+	bool AniLoad(std::string, std::string);
+	void draw(std::shared_ptr<ShaderProgram> s);
+	void update(float, float, float);
+	//used to update a portion of the skeleton.
+	//void update(int frame, int max);
+};
+
+void loadAnimations(std::shared_ptr<Holder> _temp);
---
+++ src/states.h
@@ -0,0 +1,177 @@
+#pragma once
+#include "GameState.h"
+#include "GameObject.h"
+#include "menu.h"
+#include "controller.h"
+#include "Player.h"
+#include "InputManager.h"
+#include "FrameBufferObject.h"
+#include <GLEW\glew.h>
+#include "gl\freeglut.h"
+#include "IL\ilut.h"
+// Framebuffer objects
+
+
+// Outline Controls
+
+
+//////////////////////////////////////////////////////////////////////
+///////////////////////	Lighting Controls	//////////////////////////
+enum LightingMode
+{
+	NOLIGHT,
+	TOON
+};
+
+
+
+class MainMenu : public GameState
+{
+public:
+	MainMenu(std::shared_ptr<Menu>);
+
+	void update(float dt);
+	void draw();
+	void setPaused(int);
+
+private:
+	float accept;
+	float time;
+	float timer;
+	float incrememnt;
+	int position, frame;
+	std::shared_ptr<Menu> atlas;
+};
+
+class Pause : public GameState
+{
+public:
+	Pause(std::shared_ptr<Menu>);
+	void setPaused(int);
+	void update(float dt);
+	void draw();
+	void setActive(Controller*);
+	Controller *active;
+	//used so we cant pause then immediately unpause.
+
+private:
+	float time;
+	float timer;
+
+	int position;
+	//when we pause it passes the controller used so only that player can unpause
+	std::shared_ptr<Menu> atlas;
+};
+
+class Score : public GameState
+{
+public:
+	Score(std::shared_ptr<Menu>);
+	void setPaused(int);
+	void update(float dt);
+	void draw();
+	Controller *active;
+	std::shared_ptr<Menu> atlas;
+private:
+	float accept;
+	float pauseTimer;
+};
+
+
+class Debug : public GameState
+{
+public:
+	Debug(Camera*);
+	void update(float dt);
+	void draw();
+private:
+	Camera * camera;
+};
+
+class Game : public GameState
+{
+public:
+	Game(std::map<std::string, std::shared_ptr<GameObject>>*,
+		std::map<std::string, std::shared_ptr<Player>>*,
+		std::map<std::string, std::shared_ptr<Material>>*,
+		std::shared_ptr<BombManager>,
+		Pause*, Score*, Camera*);
+
+	void update(float dt);//where we do all the updates and controls
+	void draw();// where we do all 
+	void setPaused(int a_paused);
+	void windowReshapeCallbackFunction(int w, int h);
+
+private:
+
+
+	//secondary functions (called in update and draw)
+	bool lifeCheck();
+	void updateScene(float dt);
+	void drawScene();
+	void setMaterialForAllGameObjects(std::string materialName);
+	void setMaterialForAllPlayerObjects(std::string materialName);
+	void brightPass();
+	void blurPass(FrameBufferObject fboIn, FrameBufferObject fboOut);
+	void initializeFrameBuffers();
+	void handleKeyboardInput();
+	void handleKeyboardInputShaders();
+
+	FrameBufferObject fboUnlit;
+	FrameBufferObject fboBright;
+	FrameBufferObject fboBlur, fboBlurB;
+	FrameBufferObject shadowMap;
+
+	std::shared_ptr<BombManager> bombManager;
+	std::map<std::string, std::shared_ptr<GameObject>>* scene;
+	std::map<std::string, std::shared_ptr<Player>>* players;
+	std::map<std::string, std::shared_ptr<Material>>* materials;
+	Pause* pause;
+	Score* score;
+	Camera * camera;
+	int pausing;
+	float pauseTimer;
+
+	float outlineWidth = 4.0;
+	bool outlineToggle = true;
+	LightingMode currentLightingMode = TOON;
+
+	float windowWidth = 1920.0;
+	float windowHeight = 1080.0;
+
+
+	// Lighting Controls
+	float deskLamp = 0.8;
+	float innerCutOff = 0.43; // Spot Light Size
+	float outerCutOff = 0.48;
+	glm::vec3 deskForward = glm::vec3(0.2, 1.0, 1.5); // Spot Light Direction
+	float roomLight = 0.4;
+
+	float ambient = 0.1; // Ambient Lighting
+	float diffuse = 1.0f; // Diffuse Lighting
+	float specular = 0.2f; // Specular Lighting
+	float shininess = 2.0f; // Shinniness
+	float rim = 0.5f; // Rim Lighting
+
+	GLuint toonRamp;
+
+	// Bloom Controls
+	glm::vec4 bloomThreshold = glm::vec4(0.6f);
+	int numBlurPasses = 4;
+	bool bloomToggle = false;
+
+	// For Toggling
+	bool  ambientToggle = true;
+	float ka = ambient; // Ambient Lighting
+
+	bool  diffuseToggle = true;
+	float kd = diffuse; // Diffuse Lighting
+
+	bool  specularToggle = true;
+	float ks = specular; // Specular Lighting
+
+	bool  rimToggle = true;
+	float kr = rim; // Rim Lighting
+
+};
+
