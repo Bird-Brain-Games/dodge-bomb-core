131a60c067d698c6d34860342fe7ffeebb25ca08
Graham Watson <12705535+Goombario@users.noreply.github.com>
2017-03-24 15:38:33 -0400

    in progress Players can join with the A button, when everyone on space press X issue with rigidbody and static object position set I think - Graham

---
+++ src/Player.h
@@ -0,0 +1,116 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Bomb.h"
+#include "controller.h"
+
+class readyUpRing : public GameObject
+{
+public:
+	readyUpRing(glm::vec3 position,
+		std::shared_ptr<Loader> _mesh,
+		std::shared_ptr<Material> _material,
+		std::shared_ptr<Texture> _texture,
+		int _playerNum);
+
+	inline int getPlayerNum() { return playerNum; }
+	void setPosition(glm::vec3);
+
+private:
+	int playerNum;
+};
+
+// Player class takes in player input and performs movement
+// It also handles collision between players and bombs.
+class Player : public GameObject
+{
+public:
+	enum PLAYER_STATE
+	{
+		P_INACTIVE,
+		P_NORMAL,
+		P_INVINCIBLE,
+		P_DEAD
+	};
+
+public:
+	Player(glm::vec3 position,
+		std::shared_ptr<Loader> _mesh,
+		std::shared_ptr<Material> _material,
+		std::shared_ptr<Texture> _texture, 
+		int _playerNum);
+	Player(Player&);
+	~Player();
+
+	void handleInput(float dt);
+
+	void update(float deltaT);
+	void draw(Camera &camera);
+	void attachRigidBody(std::unique_ptr<RigidBody> &_rb);
+	void attachBombManager(std::shared_ptr<BombManager> _manager);
+	void takeDamage(int damage);
+	void reset(glm::vec3 newPos);	// Reset values and positions
+
+	void checkCollisionWith(GameObject* other);
+	void checkCollisionWith(Bomb* other);
+	void checkCollisionWith(Explosion* other);
+	void checkCollisionWith(readyUpRing* other);
+	int getPlayerNum();
+	glm::vec3 getCurrentVelocity();
+
+	inline bool isReady() { return ready; }
+	inline bool isActive() { return currentState != P_INACTIVE; }
+	inline void setActive(bool active) { currentState = (active) ? P_NORMAL : P_INACTIVE; }
+
+	void setAnim(std::string);
+	int getHealth();
+
+	Controller* getController();
+
+private:
+	void lookDirectlyAtExplosion(glm::vec3 direction);
+
+private:
+
+	Controller con;
+	int playerNum;
+
+	float bombCooldown;		// The value the cooldown gets set to when thrown
+	float currentCooldown;	// The current cooldown on the bomb throw
+	std::shared_ptr<BombManager> bombManager;
+	float currentAngle;
+	float bottomAngle; //used for animation rotation.
+	glm::vec3 throwingForce;
+
+	// Player stats
+	int health;
+	PLAYER_STATE currentState;
+	bool ready;	// For ready up
+
+	// Invincibility variables
+	bool isFlashing;
+	float invincibleTime;
+	float flashTime;
+
+	// Dash variables
+	bool isDashing;
+	float currentDashCooldown;
+	float currentDashDuration;
+
+private:
+	// Player stats
+	static int maxHealth;
+	static float maxBombCooldown;
+
+	// Invincibility values
+	static float maxInvincibleTime;
+	static float pauseTime;
+	static float flashInterval;
+	static float playerSpeed;
+
+	// Dash values
+	static float dashImpulse;
+	static float dashMinSpeed;	// when the player reaches this, stop dashing
+	static float maxDashCooldown;
+	static float maxDashDuration;
+};
---
+++ src/VAO.h
@@ -0,0 +1,71 @@
+/*Campbell Hamilton 100582048
+january 2017
+*/
+
+#pragma once
+#include "gl\glew.h"
+#include <string>
+#include <vector>
+
+//attribute locations;
+enum AttributeLocations
+{
+	VERTEX = 0,
+	NORMAL,
+	TEX_COORD,
+	COLOUR,
+	JOINTS = 8,
+	WEIGHTS
+};
+
+// Contatins all the data needed to send our attributes to the shader
+class Attribute
+{
+public:
+	Attribute(AttributeLocations _attribLocation, GLenum _element, 
+		unsigned int _dataSize, unsigned int _EPA, 
+		unsigned int numElements, std::string _name, void* _data);
+
+	AttributeLocations  getAttribLocation();
+	GLenum getElement();
+	unsigned int getDataSize();
+	unsigned int getElementsPerAttrib();
+	unsigned int getNumElements();
+	std::string getAttributeName();
+	void* data;						// pointer to data
+
+
+private:
+	AttributeLocations  attribLocation;
+
+	GLenum element;					// type of data
+	unsigned int dataSize;			// size of the data
+	unsigned int elementsPerAttrib; //number of elements for one attribute (ie 3 floats for a vector)
+	unsigned int numElements;		// number of elements in entire array
+	std::string attributeName;		//name of the attribute in the shader
+
+};
+
+//contains all the attributes for an object
+class VAO
+{
+public:
+	VAO();
+	~VAO();
+
+	int addAttribute(Attribute attrib);
+	void createVAO();
+	void draw();
+	void drawLines();
+	void destroy();
+
+private:
+
+	//a handle to our VAO. which we use to tell opengl what VBO's to use
+	unsigned int vaoHandle;
+
+	//handles to our VBO. VBO's being our data we have saved to our GPU.
+	std::vector<unsigned int> vboHandles;
+	//Pointer to our arrays of data.
+	std::vector<Attribute> attributes;
+};
