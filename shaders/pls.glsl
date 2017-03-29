f78ddfe145eaa7a7319d00041c18e3148df3a5b9
rsavaglio <mibbzz@gmail.com>
2017-03-29 17:30:46 -0400

    finally got bloom going. all thanks to myself and no one else.

--- shaders/pls.glsl
+++ shaders/pls.glsl
@@ -13,14 +13,14 @@ in VertexData
 } vIn;
 
 layout(location = 0) out vec4 FragColor;
-
+in vec2 texCoord;
 
 void main()
 {
 	//////////////////////////////////////////////////////////////////////////
 	// COMPOSITE BLOOM HERE
 	////////////////////////////////////////////////////////////////////////// 
-	vec4 composite = texture(u_bright, vIn.texCoord);
+	vec4 composite =  (texture(u_bright, texCoord) * 0.4) + (texture(u_scene, texCoord));
 	FragColor = composite;
 	FragColor.a = 1.0;
 }
