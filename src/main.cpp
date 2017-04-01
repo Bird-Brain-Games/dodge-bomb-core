cbf70475802ab5c2b42d0848f142aa0b3683dc26
rsavaglio <rsavaglio@localhost>
2017-04-01 14:29:13 -0400

    Bokeh working. Outlines Buggy Still.

--- src/main.cpp
+++ src/main.cpp
@@ -98,7 +98,7 @@ void initializeShaders()
 	v_skinning.loadShaderFromFile(shaderPath + "skinning.vert", GL_VERTEX_SHADER);
 
 	// Fragment Shaders
-	Shader f_default, f_unlitTex, f_bright, f_composite, f_blur, f_texColor, f_coc,
+	Shader f_default, f_unlitTex, f_bright, f_composite, f_blur, f_texColor,
 		f_noLighting, f_toon, f_outline, f_sobel, f_shadow, f_colorCorrection, f_particles, f_bokeh, f_bokehComp;
 	f_default.loadShaderFromFile(shaderPath + "default_f.glsl", GL_FRAGMENT_SHADER);//
 	f_bright.loadShaderFromFile(shaderPath + "bright_f.glsl", GL_FRAGMENT_SHADER);//
@@ -114,7 +114,6 @@ void initializeShaders()
 	f_colorCorrection.loadShaderFromFile(shaderPath + "color_f.glsl", GL_FRAGMENT_SHADER);//
 	f_bokeh.loadShaderFromFile(shaderPath + "bokeh_f.glsl", GL_FRAGMENT_SHADER);//
 	f_bokehComp.loadShaderFromFile(shaderPath + "bokehComposite_f.glsl", GL_FRAGMENT_SHADER);//
-	f_coc.loadShaderFromFile(shaderPath + "coc_f.glsl", GL_FRAGMENT_SHADER);//
 
 	// Geometry Shaders
 	Shader g_quad, g_menu, g_particles;
@@ -213,11 +212,6 @@ void initializeShaders()
 	materials["bokehComp"]->shader->attachShader(g_quad); // Geometry Shader!
 	materials["bokehComp"]->shader->attachShader(f_bokehComp);
 	materials["bokehComp"]->shader->linkProgram();
-
-	materials["coc"] = std::make_shared<Material>("coc");
-	materials["coc"]->shader->attachShader(v_skinning);
-	materials["coc"]->shader->attachShader(f_coc);
-	materials["coc"]->shader->linkProgram();
 
 }
 
